a043ed2ebe128ee3acdae4e1e90aa3e0ceee3aa5
Ante de Baas <ante@debaas.net>
2020-07-09 23:01:18 +0200

    test

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -3,9 +3,22 @@
 # Exit immediately on non-zero return codes.
 set -e
 
+# Use the `steamcmd` command if only options were given.
+if [ "${1:0:1}" = '+' ]; then
+	set -- steamcmd "$@"
+fi
+
+
 # Run start command if only options given.
 if [[ "${1:0:1}" = '-' ]]; then
 	set -- dst-server start "$@"
+fi
+
+# Handle running the steamcmd command.
+if [ "$1" = 'steamcmd' ]; then
+
+	# Run via steam user.
+	set -- su $STEAM_USER "$@"
 fi
 
 # Run boot scripts before starting the server.
@@ -16,7 +29,7 @@ if [[ "$1" = 'dst-server' ]]; then
 	chown -R "$STEAM_USER":"$STEAM_USER" "$CLUSTER_PATH"
 
 	# Run via steam user if the command is `dst-server`.
-	set -- gosu "$STEAM_USER" "$@"
+	set -- su "$STEAM_USER" "$@"
 fi
 
 # Execute the command.
